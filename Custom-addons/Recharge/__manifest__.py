8476c2b9cc4c717d94e7e9bc0e81b110f006f099
MansiSurve <mansis.marquee@gmail.com>
2022-07-06 12:37:20 -0700

    added manifest

---
+++ Custom-addons/Recharge/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+
+{
+    'name': 'Recharge Website',
+    'version': '1.0.1',
+    'category': '',
+    'summary': '',
+    'description': """hello this is recharge website
+    """,
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/recharge.xml',
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'assets': {},
+    'license': 'LGPL-3',
+}
